d1397480ebb1b1ba773226498fca3e16f37084c0
RGuedes <geral@rolandoguedes.com>
2016-12-23 12:08:32 +0000

    checkout

--- requirejs-config.js
+++ requirejs-config.js
@@ -20,6 +20,7 @@ var config = {
             'Magento_Checkout/template/minicart/content.html':'Venture_Qooq/template/minicart/content.html',
             'Magento_Checkout/template/minicart/item/default.html':'Venture_Qooq/template/minicart/item/default.html',
             'Magento_Checkout/template/registration.html':'Venture_Qooq/template/registration.html',
+            'Magento_Checkout/template/summary.html':'Venture_Qooq/template/checkout/summary.html',
 
             'Magento_Ui/template/form/element/input.html':'Venture_Qooq/template/form/element/input.html',
             'Magento_CheckoutAgreements/template/checkout/checkout-agreements.html':'Venture_Qooq/template/checkout/checkout-agreements.html',
